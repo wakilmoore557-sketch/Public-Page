e9102ee308ac73628b83a4ddeaf8a772482d5e7a
wakilmoore557-sketch <wakilmoore557@gmail.com>
2026-04-07 06:14:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-let songs = JSON.parse(localStorage.getItem("songs")) || [];
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-function displayPublicSongs() {
+const firebaseConfig = {
+  apiKey: "AIzaSyA04iOZEmF1eL5R3nrJzEbmwuXwWrRTwOQ",
+  authDomain: "public-page-70169.firebaseapp.com",
+  databaseURL: "https://public-page-70169-default-rtdb.firebaseio.com",
+  projectId: "public-page-70169",
+  storageBucket: "public-page-70169.firebasestorage.app",
+  messagingSenderId: "220213379029",
+  appId: "1:220213379029:web:a3756acddbee7dd50fc75c"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+const songsRef = ref(db, "songs");
+
+function displaySongs(data) {
     let container = document.getElementById("songsContainer");
     container.innerHTML = "";
 
-    let publicSongs = songs.filter(song => song.isPublic);
-
-    publicSongs.forEach(song => {
+    data.forEach(song => {
         let card = document.createElement("div");
         card.className = "card";
 
@@ -20,4 +34,9 @@ function displayPublicSongs() {
     });
 }
 
-displayPublicSongs();
+onValue(songsRef, (snapshot) => {
+    let data = snapshot.val();
+    if (!data) return;
+
+    displaySongs(Object.values(data));
+});
